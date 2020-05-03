3535cdb2c0b4d9a3d9e58b2e56778dd1b58aabb7
wrunion <winterrunion@gmail.com>
2020-05-03 16:18:19 -0700

    Initial commit of the last freaking HELP QUEUE i'll ever do.

---
+++ src/components/Ticket.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Ticket(props) {
+  return (
+    <React.Fragment>
+      <h3>{props.names} {props.location}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Ticket;
