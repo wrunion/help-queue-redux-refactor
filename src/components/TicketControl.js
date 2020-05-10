738535d64bf1fd36758c2426903b67b1167f813d
wrunion <winterrunion@gmail.com>
2020-05-10 11:05:02 -0700

    Clean up TicketControl.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,20 +7,8 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: true, 
+      formVisible: false, 
       masterTicketList: [
-        {
-          names: 'Thato and Haley',
-          location: '3A',
-          issue: 'Firebase won\'t save record. Halp.',
-          id: 1
-        },
-        {
-          names: 'Sleater and Kinney',
-          location: '4B',
-          issue: 'Prop types are throwing an error.', 
-          id: 2
-        },
         {
           names: 'Imani & Jacob',
           location: '9F',
@@ -33,31 +21,35 @@ class TicketControl extends React.Component {
   }
 
   addTicketToList = (newTicket) => {
-    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
-    this.setState({masterTicketList: newMasterTicketList,
-    formVisibleOnPage: false });
+    this.setState({ 
+      masterTicketList: [...this.state.masterTicketList, newTicket], formVisible: false
+    })
   }
 
   handleClick = () => {
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      formVisible: !prevState.formVisible
     }));
   }
 
   render(){
-    let currentlyVisibleState = null;
+    const { masterTicketList, formVisible } = this.state;
+    let userView = null;
     let buttonText;
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm createTicket={this.addTicketToList}  />;
-      buttonText = "Return to Ticket List";
+
+    if (formVisible) {
+      userView = 
+        <NewTicketForm createTicket={this.addTicketToList}  />;
+        buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} />;
-      buttonText = "Add Ticket"; 
+        userView = 
+        <TicketList ticketList={masterTicketList} />;
+        buttonText = "Add Ticket"; 
     }
 
     return (
       <React.Fragment>
-        {currentlyVisibleState}
+        {userView}
         <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
